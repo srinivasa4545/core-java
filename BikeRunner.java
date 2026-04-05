b4f22b66f965597463d4d8892673e3be53f16362
srinivasa4545 <sheshashesha4545@gmail.com>
2026-04-05 12:56:22 +0530

    method

---
+++ BikeRunner.java
@@ -0,0 +1,11 @@
+class BikeRunner{
+ public static void main(string[] args){
+
+Bike.getBike();
+bike.bikeInfo("KTM");
+String Info=bike.bikeDetails();
+System.out.println(Info);
+String Info=bike.getbikeDetails("KTM");
+Sysytem.out.println(Info);
+}
+}
